79207cb5cb537bf273d2417e8c1669f94733aeaa
mapaute2 <73542451+mapaute2@users.noreply.github.com>
2022-11-03 20:28:27 -0500

    Add files via upload

---
+++ pets-main/index.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Happy Pets</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="icon" href="images/logo.png" sizes="32x32" />
+    <link rel="icon" href="images/logo.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="images/logo.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+<div class="contenedor">
+	<header class="cabeceraPrincipal">
+		<section class="logotipo"><img src="images/logotipoPet.png"></section>
+		<nav class="menuPrincipal">
+			<a href="#">Inicio</a>
+			<a href="#">Servicios</a>
+			<a href="#">Productos</a>
+			<a href="#">Adopción</a>
+			<a href="#">Contactos</a>
+		</nav>
+	</header>
+	<section class="titulo">
+		<h2>Adopcion</h2>
+	</section>
+	<section class="sliderPrincipal"><img src="images/indice.jpg">/section>
+	<section class="adopcion1"><h3>Noticia 1</h3></section>
+	<section class="adopcion2"><h3>Noticia 2</h3></section>
+	<section class="doctor">
+		<h3>Doctores</h3>
+	</section>
+	<section class="sponsor">
+		<h3>Sponsor</h3>
+	</section>
+	<footer class="piePagina">
+		<section class="derechos">
+			<h6>Derechos Reservados UTPL 2022</h6>
+		</section class="redesSociales">
+		<nav class="redesSociales"> <!--se usa en menu-->
+			<a href="#"><i class="fa-brands fa-facebook"></i></a> 
+			<a href="#"><i class="fa-brands fa-instagram"></i></a>
+			<a href="#"><i class="fa-brands fa-twitter"></i></a>
+		</nav>
+	</footer>
+</div>
+</body>
+</html>
